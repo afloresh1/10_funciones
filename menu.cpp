2bee6d9993ec891c9d31f000f8e78d49c0d565e3
jarocutipam <lhanccoc@unjbg.edu.pe>
2024-06-24 08:19:35 -0500

    modificacion del menu

--- menu.cpp
+++ menu.cpp
@@ -7,8 +7,8 @@ int main(){
     cout<<"1. Depositar "<<endl;
     cout<<"2.Retirar "<<endl;
     cout<<"3. Mostrar saldo "<<endl;
-    cout<<"0.Salir "<<endl;
-    cout<<"Elija una opcion: "; cin>>op;
+    cout<<"0. Salir "<<endl;
+    cout<<"Elija una opcion : "; cin>>op;
     switch (op){
         case  0:
             cout<<"Saliendo ... "<<endl;
